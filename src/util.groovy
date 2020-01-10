fdfc0d8354944c7bbb8e39384347b947881a2fa1
Li Lin <guru.lin@gmail.com>
2020-01-11 03:17:37 +0800

    fix syntax error

--- src/util.groovy
+++ src/util.groovy
@@ -7,7 +7,7 @@ def envForBranch(string branchName) {
 	branch = build.getBuildVariables().get('GIT_BRANCH')
   	if (branch == 'develop') {
   		return 'dev'
-  	} elif (branch.startsWith('release/')) {
+  	} else if (branch.startsWith('release/')) {
   		return 'sit'
   	} else {
   		return 'any'
