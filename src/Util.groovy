6d3a5b646ed7e141455552af4664091f090c17c0
Li Lin <guru.lin@gmail.com>
2020-01-11 04:13:25 +0800

    fix syntax error

--- src/Util.groovy
+++ src/Util.groovy
@@ -1,8 +1,10 @@
 class Util implements Serializable {
 
   static def envForBranch(String branch) {
-    //System.out.println(String.format("*** %s ***"), branch)
-    branch = System.getenv("GIT_BRANCH")
+    if (branch == null || branch == '' ) {
+      return 'emtpy'
+    }
+    
   	 if (branch == 'develop') {
   		  return 'dev'
   	 } else if (branch.startsWith('release/')) {
